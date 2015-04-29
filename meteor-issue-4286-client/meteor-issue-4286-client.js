2f22dd06a1d7bdc014f0a2ba6943f62cb7fdc05a
lizunlong <lizunlong@gmail.com>
2015-04-29 23:24:37 +0800

    Initial commit

---
+++ meteor-issue-4286-client/meteor-issue-4286-client.js
@@ -0,0 +1,27 @@
+connection = DDP.connect("http://localhost:3000");
+Accounts.connection = connection;
+// Meteor.users = new Mongo.Collection("users", {connection: connection});
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
